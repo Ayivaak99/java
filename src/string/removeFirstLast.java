bb165976d46f796ffa5cde8154a3653700add8c4
Ayivaak99 <prkaaviya17@gmail.com>
2020-08-07 09:24:07 +0530

    initialised git repo with 5 classes of type string

---
+++ src/string/removeFirstLast.java
@@ -0,0 +1,12 @@
+package string;
+
+public class removeFirstLast {
+    public static String apply(String str) {
+        return str.substring(1, str.length()-1);
+    }
+
+    public static void main(String[] args) {
+        String str = "Remove first and last characters from the string";
+        System.out.println(apply(str));
+    }
+}
