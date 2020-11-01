a5f354f6a3c09e6129c88dc77d5685d53479317e
Kaaviya Ramkumar <prkaaviya17@gmail.com>
2020-11-01 23:28:08 +0530

    Add interface functionality

---
+++ java/src/tutorials/masterclass/interfaceExample/iTelephone.java
@@ -0,0 +1,9 @@
+package tutorials.masterclass.interfaceExample;
+
+public interface iTelephone {
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
+}
