a89b81b5c5eef6fd1dceb249733046bce2274506
Kaaviya Ramkumar <prkaaviya17@gmail.com>
2020-11-22 21:38:26 +0530

    Add generics tutorial files

---
+++ java/src/main/java/tutorials/masterclass/generics/Main2.java
@@ -0,0 +1,24 @@
+package main.java.tutorials.masterclass.generics;
+
+public class Main2 {
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(joe);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
+
+        Team<BaseballPlayer> chicagoCubs = new Team<>("Chicago Cubs");
+        chicagoCubs.addPlayer(pat);
+
+        Team<SoccerPlayer> barcelona = new Team<>("Barcelona");
+        barcelona.addPlayer(beckham);
+
+        System.out.println(adelaideCrows.numPlayers());
+
+    }
+}
