1d7b8fcb2aa382af7a2729b05d9728f947b04d82
Kaaviya Ramkumar <prkaaviya17@gmail.com>
2020-12-09 12:51:58 +0530

    Add sortList method in collections module

--- java/src/main/tutorials/masterclass/collections/Main.java
+++ java/src/main/tutorials/masterclass/collections/Main.java
@@ -24,6 +24,11 @@ public class Main {
         printList(seatCopy);
         System.out.println("Printing theatre.seats");
         printList(theatre.seats);
+
+        sortList(seatCopy);
+        System.out.println("Printing sorted seatCopy");
+        printList(seatCopy);
+
 //        theatre.getSeats();
 //        if (theatre.reserveSeat("B14")) {
 //            System.out.println("Please pay");
@@ -41,10 +46,21 @@ public class Main {
     }
 
     public static void printList(List<Theatre.Seat> list) {
+        System.out.println("#########################################");
         for (Theatre.Seat seat : list) {
             System.out.print(" " + seat.getSeatNumber());
         }
         System.out.println();
         System.out.println("#########################################");
+    }
+
+    public static void sortList(List<? extends Theatre.Seat> list) {
+        for (int i = 0; i< list.size() - 1; i++){
+            for (int j= i+1; j< list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
     }
 }
